f577c1c8ce1575b9926784d05ba688fb750c647b
Max Bladen-Clark <max@buzzard.dev>
2019-12-17 16:44:19 +0000

    add fake upstream for adding arrivals

--- __tests_/src/lib/fake-upstream.spec.js
+++ __tests_/src/lib/fake-upstream.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {getCaptain} = require('../../../src/lib/fake-upstream');
+const {getCaptain, addArrival, JOURNEYS} = require('../../../src/lib/fake-upstream');
 
 describe('get captain', () => {
     test('it should return undefined if the captain does not exist', () => {
@@ -36,4 +36,99 @@ describe('get captain', () => {
         expect(getCaptain('patsy stOne')).not.toBeUndefined();
         expect(getCaptain('patsY sTone')).not.toBeUndefined();
     })
+});
+
+describe('add arrival', () => {
+    test('it should throw an error if the captain does not exist', () => {
+        let err;
+        try {
+            addArrival({
+                captain: 'Captain America',
+                vessel: 'El Tauro',
+                datetime: '5th of Feb 2056',
+                port: 'Singapore'})
+        } catch(e) {
+            err = e
+        }
+        expect(err).toHaveProperty('message', 'Captain not Found')
+    });
+    test('it should throw if the journey is missing properties', () => {
+        let err;
+        try {
+            addArrival({
+                captain: 'Patsy Stone',
+                vessel: 'El Tauro',
+                datetime: '5th of Feb 2056'
+            })
+        } catch(e) {
+            err = e
+        }
+        expect(err).toHaveProperty('message', 'Missing Properties')
+    });
+    test('it should throw if the vessel has no previous history', () => {
+        let err;
+        try {
+            addArrival({
+                captain: 'Patsy Stone',
+                vessel: 'SS Penguin',
+                datetime: '5th of Feb 2056',
+                port: 'Singapore'
+            })
+        } catch(e) {
+            err = e
+        }
+        expect(err).toHaveProperty('message', 'Vessel not Found')
+    });
+    test('it should throw if the datetime is not in the expected format', () => {
+        let err;
+        try {
+            addArrival({
+                captain: 'Patsy Stone',
+                vessel: 'El Tauro',
+                datetime: '2056/02/05',
+                port: 'Singapore'
+            })
+        } catch(e) {
+            err = e
+        }
+        expect(err).toHaveProperty('message', 'Bad date format')
+    });
+    test('it should throw if the datetime is before the last reported date for the vessel', () => {
+        let err;
+        try {
+            addArrival({
+                captain: 'Patsy Stone',
+                vessel: 'El Tauro',
+                datetime: '5th of Feb 2000',
+                port: 'Singapore'
+            })
+        } catch(e) {
+            err = e
+        }
+        expect(err).toHaveProperty('message', 'Date too early')
+    });
+    describe('when the captain exists and the journey is correctly formatted', () => {
+        let result;
+        beforeAll(() => {
+            result = addArrival({
+                captain: 'Patsy Stone',
+                vessel: 'El Tauro',
+                datetime: '5th of Feb 2056',
+                port: 'Singapore'
+            })
+        });
+        test('it should return true', () => {
+            expect(result).toEqual(true)
+        });
+        test('it adds a journey to the journey\'s array with the fromDate being the vessel\'s last arrival in a port', () => {
+            expect(JOURNEYS).toContainEqual({
+                captain: 'Patsy Stone',
+                vessel: 'El Tauro',
+                from: 'Melbourne',
+                to: 'Singapore',
+                fromDate: 1515888000000,
+                toDate: 2716934400000
+            })
+        })
+    });
 });
--- src/lib/fake-upstream.js
+++ src/lib/fake-upstream.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const moment = require('moment');
+
 const CAPTAINS = ["Patsy Stone"];
-const JOURNEYS = [
+const JOURNEYS = exports.JOURNEYS = [
     {
         captain: "Patsy Stone",
         vessel: 'El Tauro',
@@ -22,4 +24,32 @@ exports.getCaptain = (name) => {
                 .map(({vessel, from, to, fromDate, toDate}) => ({vessel, from, to, fromDate, toDate}))
         }
     }
+};
+
+exports.addArrival = ({captain: captainName, vessel, datetime, port} = {}) => {
+    if (!CAPTAINS.find(captain => captain.toLowerCase() === captainName.toLowerCase())) throw new Error('Captain not Found');
+    if ([vessel, datetime, port].includes(undefined)) throw new Error('Missing Properties');
+
+    // Parse Datetime
+    const parsedDateTime = moment(datetime, 'Do [of] MMM YYYY');
+    if (parsedDateTime.format('Do [of] MMM YYYY') !== datetime) throw new Error('Bad date format');
+
+    // Get Previous Journey for Vessel
+    const js = JOURNEYS.filter((j) => vessel === j.vessel).sort((a, b) => b.toDate - a.toDate);
+    const [previousJourney] = js;
+    if (!previousJourney) throw new Error('Vessel not Found');
+    if (previousJourney.toDate >= parsedDateTime.unix() * 1000) throw new Error('Date too early');
+
+
+    const journey = {
+        captain: CAPTAINS.find(captain => captain.toLowerCase() === captainName.toLowerCase()),
+        vessel,
+        from: previousJourney.to,
+        to: port,
+        fromDate: previousJourney.toDate,
+        toDate: moment(datetime, 'Do [of] MMM YYYY').unix() * 1000
+    };
+
+    JOURNEYS.push(journey);
+    return true
 };
