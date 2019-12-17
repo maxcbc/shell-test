8788baebb42ed789620b0a2fdf9ed006816019d8
Max Bladen-Clark <max@buzzard.dev>
2019-12-17 19:37:17 +0000

    add api middleware to index

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = module.exports = express();
 const {PORT = 8080} = process.env;
 
+app.use(require('./src/middleware/api-key'));
+app.use(require('body-parser').json());
 app.get('/history/:captainName', require('./src/controllers/get-history'));
 
 app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
