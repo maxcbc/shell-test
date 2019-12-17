04bb088073fed7b94fa7a973a19379d3a45854d1
Max Bladen-Clark <max@buzzard.dev>
2019-12-17 07:54:25 +0000

    Implement basic server in express

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
-// TODO: implement
+const express = require('express');
+
+const app = module.exports = express();
+const {PORT = 8080} = process.env;
+
+app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
